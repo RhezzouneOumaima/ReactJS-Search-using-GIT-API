d7c1c238bcc79114c14645db51ea3e846d7207e5
OUMAIMA RHEZZOUNE <60359694+RhezzouneOumaima@users.noreply.github.com>
2022-08-10 14:51:32 +0200

    first try

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>salaaaam</h1>
+      <a href="" className="btn btn-primary">salaam</a>
+    </div>
+  );
+}
+
+export default App;
